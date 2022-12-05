5028c09acab571f1a6657409a4234be30b62f0ba
NyokabiKamau <nyokabikamau2014@gmail.com>
2022-12-05 11:13:55 +0300

    Done with assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("form").addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleToDo(e.target.value)
+    document.querySelector('form').reset();
+  });
 });
+
+function handleToDo(task) {
+  const input = document.getElementById("new-task-description");
+  let li = document.createElement("li");
+  let btn = document.createElement("button")
+  btn.addEventListener('click', deleteTask)
+  btn.textContent = 'x';
+  li.innerHTML = input.value;
+  li.appendChild(btn)
+  document.querySelector("#tasks").appendChild(li);
+}
+
+function deleteTask(e) {
+  e.target.parentNode.remove();
+}
